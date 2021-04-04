39a99d4b70f0122fb2ab7ff9f7c11a97a3ccadba
Sean Wei <me@sean.taipei>
2021-04-04 20:19:59 +0800

    Apr 04  20:19

--- Lab1/alu.v
+++ Lab1/alu.v
@@ -1,36 +1,18 @@
 `timescale 1ns/1ps
 
-//////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer:
-//
-// Create Date:    15:15:11 08/18/2013
-// Design Name:
-// Module Name:    alu
-// Project Name:
-// Target Devices:
-// Tool versions:
-// Description:
-//
-// Dependencies:
-//
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-//
-//////////////////////////////////////////////////////////////////////////////////
+// Author: 0816146 韋詠祥
 
 module alu(
-           clk,           // system clock              (input)
-           rst_n,         // negative reset            (input)
-           src1,          // 32 bits source 1          (input)
-           src2,          // 32 bits source 2          (input)
-           ALU_control,   // 4 bits ALU control input  (input)
-           result,        // 32 bits result            (output)
-           zero,          // 1 bit when the output is 0, zero must be set (output)
-           cout,          // 1 bit carry out           (output)
-           overflow       // 1 bit overflow            (output)
-           );
+	clk,           // system clock              (input)
+	rst_n,         // negative reset            (input)
+	src1,          // 32 bits source 1          (input)
+	src2,          // 32 bits source 2          (input)
+	ALU_control,   // 4 bits ALU control input  (input)
+	result,        // 32 bits result            (output)
+	zero,          // 1 bit when the output is 0, zero must be set (output)
+	cout,          // 1 bit carry out           (output)
+	overflow       // 1 bit overflow            (output)
+);
 
 input           clk;
 input           rst_n;
@@ -48,12 +30,46 @@ reg             zero;
 reg             cout;
 reg             overflow;
 
-always@( posedge clk or negedge rst_n )
-begin
-	if(!rst_n) begin
+reg Ai, Bi;
+reg [1:0] op;
+reg [31:0] less, cin;
+wire [31:0] res, c_out;
 
-	end
-	else begin
+genvar i;
+generate for (i=0; i<32; i=i+1)
+	alu_top u0(src1[i], src2[i], less[i], Ai, Bi, cin[i], op, res[i], c_out[i]);
+endgenerate
+
+always @(posedge clk or negedge rst_n) begin
+	if (!rst_n) begin
+		Ai <= 0;
+		Bi <= 0;
+		op <= 2'b00;
+		cin <= 32'b0;
+		less <= 32'b0;
+	end else begin
+		zero <= ~(|res);
+		result <= res;
+
+		case (ALU_control)
+			4'b0000: begin
+				op <= 2'b00;
+				Ai <= 0;
+				Bi <= 0;
+				cout <= 0;
+				overflow <= 0;
+			end
+			4'b0001: begin
+				op <= 2'b01;
+				Ai <= 0;
+				Bi <= 0;
+				cout <= 0;
+				overflow <= 0;
+			end
+			default: begin
+				overflow <= 1;
+			end
+		endcase
 
 	end
 end
--- Lab1/alu_top.v
+++ Lab1/alu_top.v
@@ -1,39 +1,19 @@
 `timescale 1ns/1ps
 
-//////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer:
-//
-// Create Date:    10:58:01 10/10/2013
-// Design Name:
-// Module Name:    alu_top
-// Project Name:
-// Target Devices:
-// Tool versions:
-// Description:
-//
-// Dependencies:
-//
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-//
-//////////////////////////////////////////////////////////////////////////////////
-
 module alu_top(
-               src1,       //1 bit source 1 (input)
-               src2,       //1 bit source 2 (input)
-               less,       //1 bit less     (input)
-               A_invert,   //1 bit A_invert (input)
-               B_invert,   //1 bit B_invert (input)
-               cin,        //1 bit carry in (input)
-               operation,  //operation      (input)
-               result,     //1 bit result   (output)
-               cout,       //1 bit carry out(output)
-               );
+	src1_in,       //1 bit source 1 (input)
+	src2_in,       //1 bit source 2 (input)
+	less,       //1 bit less     (input)
+	A_invert,   //1 bit A_invert (input)
+	B_invert,   //1 bit B_invert (input)
+	cin,        //1 bit carry in (input)
+	operation,  //operation      (input)
+	result,     //1 bit result   (output)
+	cout       //1 bit carry out(output)
+);
 
-input         src1;
-input         src2;
+input         src1_in;
+input         src2_in;
 input         less;
 input         A_invert;
 input         B_invert;
@@ -45,9 +25,22 @@ output        cout;
 
 reg           result;
 
-always@( src1_temp or src2_temp or operation )
-begin
+wire          src1, src2;
+assign src1 = src1_in ^ A_invert;
+assign src2 = src2_in ^ B_invert;
+assign cout = (operation == 2'b10) & src1 & src2;
 
+always @(src1 or src2 or operation) begin
+	case (operation)
+		2'b00:
+			result <= src1 & src2;
+		2'b01:
+			result <= src1 | src2;
+		2'b10:
+			result <= src1 ^ src2;
+		2'b11:
+			result <= 1;
+	endcase
 end
 
 endmodule
