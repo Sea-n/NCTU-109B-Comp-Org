7201187b45c10ff6c5f71d89a80b406eb9a2b4d5
Sean Wei <me@sean.taipei>
2021-04-24 22:07:45 +0800

    Implement 2x1 MUX

--- Lab2-CPU/MUX_2to1.v
+++ Lab2-CPU/MUX_2to1.v
@@ -17,5 +17,11 @@ input select_i;
 output reg [size-1:0] data_o;
 
 // Main function
+always @(select_i, data1_i, data2_i) begin
+	if (select_i)
+		data_o <= data1_i;
+	else
+		data_o <= data0_i;
+end
 
 endmodule
