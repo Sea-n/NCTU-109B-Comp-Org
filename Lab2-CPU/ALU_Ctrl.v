9a9d0d6d88ac78de4c94d3d55e39e572ad34e876
Sean Wei <me@sean.taipei>
2021-04-24 20:42:39 +0800

    Implement ALU control

--- Lab2-CPU/ALU_Ctrl.v
+++ Lab2-CPU/ALU_Ctrl.v
@@ -11,10 +11,14 @@ module ALU_Ctrl(
 // I/O ports
 input [6-1:0] funct_i;
 input [3-1:0] ALUOp_i;
-output reg [4-1:0] ALUCtrl_o;
+output [4-1:0] ALUCtrl_o;
 
 // Parameter
 
 // Select exact operation
+assign ALUCtrl_o[3] = 0;
+assign ALUCtrl_o[2] = ALUOp_i[2] | ALUOp_i[1] | funct_i[1];
+assign ALUCtrl_o[1] = ~funct_i[2];
+assign ALUCtrl_o[0] = ALUOp_i[1] | (funct_i[3] ^ funct_i[0]);
 
 endmodule
