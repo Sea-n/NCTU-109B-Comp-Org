9d9421cea019b731f4427cb69bf9c0860aa667cb
Sean Wei <me@sean.taipei>
2021-06-25 12:41:26 +0800

    Add report

--- Lab6-Cache-Simulator/plot.py
+++ Lab6-Cache-Simulator/plot.py
@@ -5,7 +5,7 @@ from subprocess import run, PIPE
 
 def drawA():
     x = [str(2**i) + ' bytes' for i in range(3, 8)]
-    z = [str(2**i) + ' KB' for i in range(2, 7)]
+    z = [str(2**i) + ' KB' for i in range(2, 6)] + ['512 KB']
 
     for c in z:
         val = []
@@ -17,7 +17,7 @@ def drawA():
         print(c, val)
 
     plt.legend(title='Cache size', loc='upper right')
-    plt.title('Associativity = direct_map_cache')
+    plt.title('Associativity = 1 (direct map cache)')
     plt.xlabel('Block size')
     plt.ylabel('Miss rate')
     plt.ylim(0, 30)
@@ -28,8 +28,8 @@ def drawA():
 
 
 def drawB():
-    x = [str(2**i) + '-way' for i in range(4)]
-    z = [str(2**i) + ' KB' for i in range(6)]
+    x = [str(2**i) + '-way' for i in range(3)] + ['32-way']
+    z = [str(2**i) + ' KB' for i in range(6)] + ['512 KB']
 
     for c in z:
         val = []
@@ -41,19 +41,47 @@ def drawB():
         print(c, val)
 
     plt.legend(title='Cache size', loc='upper right')
-    plt.title('Block size = 32 byte')
+    plt.title('Block Size = 32 bytes')
     plt.xlabel('Associativity')
     plt.ylabel('Miss rate')
     plt.ylim(0, 30)
     plt.xticks(x)
 
     plt.savefig('figure2.png')
+    plt.cla()
+
+
+def drawC():
+    w = [str(2**i) + ' KB' for i in range(3, 6)] + ['512 KB']
+    x = [str(2**i) + ' bytes' for i in range(3, 9)]
+    z = [str(2**i) + '-way' for i in range(4)]
+
+    for c in w:
+        for a in z:
+            val = []
+            for b in x:
+                v = run(['./cache_simulator', '-f', 'Trace.txt', '-c', c,
+                         '-b', b, '-a', a, '-p', '4'], stdout=PIPE)
+                val.append(float(v.stdout))
+            plt.plot(x, val, marker='o', label=a)
+            print(a, val)
+
+        plt.legend(title='Associativity', loc='upper right')
+        plt.title(f'Cache Size = {c}')
+        plt.xlabel('Block size')
+        plt.ylabel('Miss rate')
+        plt.ylim(0, 30)
+        plt.xticks(x)
+
+        plt.savefig(f'figure3-{c}.png')
+        plt.cla()
 
 
 def main():
     plt.figure(dpi=320)
     drawA()
     drawB()
+    drawC()
 
 
 if __name__ == '__main__':
