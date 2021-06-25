10119fc9187c13cc43818fd3475da8756a9a798e
Sean Wei <me@sean.taipei>
2021-06-25 10:47:49 +0800

    Finish plot script

--- Lab6-Cache-Simulator/plot.py
+++ Lab6-Cache-Simulator/plot.py
@@ -1,50 +1,60 @@
+#!/usr/bin/env python3
 import matplotlib.pyplot as plt
 from subprocess import run, PIPE
 
 
 def drawA():
-    x = [str(2**i) for i in range(1, 6)]
+    x = [str(2**i) + ' bytes' for i in range(3, 8)]
+    z = [str(2**i) + ' KB' for i in range(2, 7)]
 
-    for c in [64, 128, 256, 512]:
+    for c in z:
         val = []
         for b in x:
-            v = run(["./cache_simulator", "-f", "Trace.txt", "-c", str(c),
-                     "-b", str(b), "-a", "1", "-d", "4"], stdout=PIPE)
+            v = run(['./cache_simulator', '-f', 'Trace.txt', '-c', c,
+                     '-b', b, '-a', '1', '-p', '4'], stdout=PIPE)
             val.append(float(v.stdout))
-        print("c=", c, "val=", val)
-        plt.plot(x, val, marker="o", label=c)
+        plt.plot(x, val, marker='o', label=c)
+        print(c, val)
 
-    plt.title("Associativity = direct_map_cache")
-    plt.xlabel("Block size (byte)")
-    plt.ylabel("Miss rate")
+    plt.legend(title='Cache size', loc='upper right')
+    plt.title('Associativity = direct_map_cache')
+    plt.xlabel('Block size')
+    plt.ylabel('Miss rate')
+    plt.ylim(0, 30)
     plt.xticks(x)
-    plt.ylim(0, 15)
-    plt.legend(title="Cache size (KB)")
 
-    plt.show()
+    plt.savefig('figure1.png')
+    plt.cla()
 
 
 def drawB():
-    x = [str(2**i) + "-way" for i in range(4)]
+    x = [str(2**i) + '-way' for i in range(4)]
+    z = [str(2**i) + ' KB' for i in range(6)]
 
-    for c in [str(2**i) for i in range(6)]:
+    for c in z:
         val = []
         for a in x:
-            v = run(["./cache_simulator", "-f", "Trace.txt", "-c", str(c),
-                     "-b", "32", "-a", a, "-d", "4"], stdout=PIPE)
+            v = run(['./cache_simulator', '-f', 'Trace.txt', '-c', c,
+                     '-b', '32', '-a', a, '-p', '4'], stdout=PIPE)
             val.append(float(v.stdout))
-        print("c=", c, "val=", val)
-        plt.plot(x, val, marker="o", label=c)
+        plt.plot(x, val, marker='o', label=c)
+        print(c, val)
 
-    plt.title("Block size = 32 byte")
-    plt.xlabel("Associativity")
-    plt.ylabel("Miss rate")
+    plt.legend(title='Cache size', loc='upper right')
+    plt.title('Block size = 32 byte')
+    plt.xlabel('Associativity')
+    plt.ylabel('Miss rate')
+    plt.ylim(0, 30)
     plt.xticks(x)
-    plt.ylim(0, 25)
-    plt.legend(title="Cache size (KB)")
 
-    plt.show()
+    plt.savefig('figure2.png')
 
 
-drawA()
-drawB()
+def main():
+    plt.figure(dpi=320)
+    drawA()
+    drawB()
+
+
+if __name__ == '__main__':
+    main()
